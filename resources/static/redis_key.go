370972690fa302f2611026ce36b4e1765a1b29f8
NW231208R\n-kimura <n-kimura@clinks.jp>
2024-02-04 23:39:20 +0900

    google meet 第一段階

--- resources/static/redis_key.go
+++ resources/static/redis_key.go
@@ -2,6 +2,7 @@ package static
 
 const (
 	REDIS_USER_HASH_KEY       string = "hash_key"
+	REDIS_APPLICANT_HASH_KEY  string = "applicant_hash_key"
 	REDIS_CODE                string = "code"
 	REDIS_S3_NAME             string = "s3_name"
 	REDIS_OAUTH_REFRESH_TOKEN string = "oauth_refresh_token"
